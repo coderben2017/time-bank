4447525e001d9142838fde2f3ca7125e5402960d
jinben <jinben@gagogroup.com>
2017-12-29 09:53:14 +0800

    feat(components/login): adjust login check for static show

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,15 +29,22 @@ export class LoginComponent implements OnInit {
    * 用户登录
    */
   login(): void {
-    this.loginService.login(this.loginFormGroup.value.username, this.loginFormGroup.value.password).subscribe(res => {
-      if (res) {
-        this.router.navigateByUrl('/dashboard/community/plans');
-        window.sessionStorage.setItem('usr', this.loginFormGroup.value.username);
-        window.location.reload();
-      } else {
-        alert('账号或密码错误');
-      }
-    });
+    // this.loginService.login(this.loginFormGroup.value.username, this.loginFormGroup.value.password).subscribe(res => {
+    //   if (res) {
+        // this.router.navigateByUrl('/dashboard/community/plans');
+        // window.sessionStorage.setItem('usr', this.loginFormGroup.value.username);
+        // window.location.reload();
+    //   } else {
+    //     alert('账号或密码错误');
+    //   }
+    // });
+    if (this.loginFormGroup.value.username === 'admin' && this.loginFormGroup.value.password === 'admin') {
+      this.router.navigateByUrl('/dashboard/community/plans');
+      window.sessionStorage.setItem('usr', this.loginFormGroup.value.username);
+      window.location.reload();
+    } else {
+      alert('账号或密码错误，请重新输入');
+    }
   }
 
 }
